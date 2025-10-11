c792d22ecb3b7cce4f41c3d4d01b4b3c1fe65018
Alexander Kozharsky <a.kozharsky@icloud.com>
2025-10-11 20:27:56 +0300

    Includes fixes.

--- example/xmake.lua
+++ example/xmake.lua
@@ -7,7 +7,7 @@ add_rules('mode.debug', 'mode.release')
 add_requires('omsfw 1.0.0')
 
 target('omsfw-example')
-  add_packages('omsfw')
   set_kind('binary')
   add_files('src/*.mm', 'src/*.m')
   add_includedirs('src/include')
+  add_packages('omsfw')
--- src/include/QweController.h
+++
@@ -1,9 +0,0 @@
-#import <ObjFW/ObjFW.h>
-#import "OMSFW.h"
-
-@interface QweController : OMSFWController
-
-+ (instancetype)controller;
-- (instancetype)init OF_DESIGNATED_INITIALIZER;
-
-@end
